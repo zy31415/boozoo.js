6035e6e7b79f6636452e5423759d4ed6415103d1
Yang Zhang <zy31415@gmail.com>
2017-07-03 16:19:17 -0700

    .

--- main.js
+++ main.js
@@ -1,41 +1,70 @@
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, Menu} = require('electron');
 const path = require('path');
 const url = require('url');
 
+
 const Datastore = require('nedb');
 
-let db = new Datastore({filename: './temp/datafile.db'});
+let db = new Datastore({filename: './temp/boozoo.datafile.json'});
 
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win;
 
+app.on('ready', () => {
+    createWindow();
+});
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+});
+
+app.on('activate', () => {
+    // On macOS it's common to re-create a window in the src when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+        createWindow()
+    }
+});
+
 function createWindow () {
     // Create the browser window.
-    win = new BrowserWindow({width: 800, height: 600})
+    win = new BrowserWindow({width: 800, height: 600});
 
-    // and load the index.html of the app.
+    // and load the index.html of the src.
     win.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
+        pathname: path.join(__dirname, 'src/index.html'),
         protocol: 'file:',
         slashes: true
-    }))
+    }));
 
-    // Open the DevTools.
-    win.webContents.openDevTools();
+    win.once('ready-to-show', () => {
+       win.show();
+    });
 
     // Emitted when the window is closed.
     win.on('closed', () => {
         // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
+        // in an array if your src supports multi windows, this is the time
         // when you should delete the corresponding element.
         win = null
     });
 
+    Menu.setApplicationMenu(
+        Menu.buildFromTemplate(require('./src/menu').template)
+    );
+
     db.loadDatabase(function (err) {
-        console.log(err);
-        console.log("Database error!");
+        if (err !== null) {
+            console.log("Database error!");
+            console.log(err);
+        }
     });
 
     let doc = { hello: 'world'
@@ -54,29 +83,3 @@ function createWindow () {
     });
 
 }
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
-
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-    app.quit()
-}
-})
-
-app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (win === null) {
-    createWindow()
-}
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
-
---
+++ src/menu.js
@@ -0,0 +1,127 @@
+/**
+ * Created by zy on 7/3/17.
+ */
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require('path');
+const url = require('url');
+
+const template = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Settings ...',
+                click: setDataFile
+            }
+        ]
+    },
+    {
+        label: 'Edit',
+        submenu: [
+            {role: 'undo'},
+            {role: 'redo'},
+            {type: 'separator'},
+            {role: 'cut'},
+            {role: 'copy'},
+            {role: 'paste'},
+            {role: 'pasteandmatchstyle'},
+            {role: 'delete'},
+            {role: 'selectall'}
+        ]
+    },
+    {
+        label: 'View',
+        submenu: [
+            {role: 'reload'},
+            {role: 'forcereload'},
+            {role: 'toggledevtools'},
+            {type: 'separator'},
+            {role: 'resetzoom'},
+            {role: 'zoomin'},
+            {role: 'zoomout'},
+            {type: 'separator'},
+            {role: 'togglefullscreen'}
+        ]
+    },
+    {
+        role: 'window',
+        submenu: [
+            {role: 'minimize'},
+            {role: 'close'}
+        ]
+    },
+    {
+        role: 'help',
+        submenu: [
+            {
+                label: 'Learn More',
+                click () { require('electron').shell.openExternal('https://electron.atom.io') }
+            }
+        ]
+    }
+];
+
+if (process.platform === 'darwin') {
+    template.unshift({
+        label: app.getName(),
+        submenu: [
+            {role: 'about'},
+            {type: 'separator'},
+            {role: 'services', submenu: []},
+            {type: 'separator'},
+            {role: 'hide'},
+            {role: 'hideothers'},
+            {role: 'unhide'},
+            {type: 'separator'},
+            {role: 'quit'}
+        ]
+    })
+
+    // Edit menu
+    template[1].submenu.push(
+        {type: 'separator'},
+        {
+            label: 'Speech',
+            submenu: [
+                {role: 'startspeaking'},
+                {role: 'stopspeaking'}
+            ]
+        }
+    )
+
+    // Window menu
+    template[3].submenu = [
+        {role: 'close'},
+        {role: 'minimize'},
+        {role: 'zoom'},
+        {type: 'separator'},
+        {role: 'front'}
+    ]
+}
+
+module.exports.template = template;
+
+function setDataFile (item, focusedWindow) {
+    console.log("Settings..");
+    // require('electron').dialog.showOpenDialog();
+
+    let settings = new BrowserWindow(
+        {
+            parent: focusedWindow,
+            title: "Settings",
+            width: 400,
+            height: 225
+        }
+    );
+
+    settings.loadURL(url.format({
+        pathname: path.join(__dirname, './settings.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    settings.show();
+
+}
